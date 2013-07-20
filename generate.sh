2c021de5a51b945a9e62ebe8a85f0276a95ca243
Kevin Yap <me@kevinyap.ca>
2013-07-19 21:43:55 -0700

    generate.sh does a dry-run of rsync to confirm transfers and also removes .DS_Store files

--- generate.sh
+++ generate.sh
@@ -4,26 +4,28 @@ pelican -s ./settings.py
 cp favicon.ico output/favicon.ico && echo "favicon.ico successfully copied."
 cp robots.txt output/robots.txt && echo "robots.txt successfully copied."
 cp .htaccess output/.htaccess && echo ".htaccess successfully copied."
+find . -name '*.DS_Store' -type f -delete && echo "Removed .DS_Store files recursively."
+
+printf "\e[0;32mSite generated successfully.\e[0m\n"
 
 # If -u flag is used, upload output folder via rsync
 if [ -z "$1" ]; then
-	echo -e "Site generated successfully."
 	exit
 elif [ $1 = "-u" ]; then
-	echo "Site generated successfully."
+	echo "Beginning dry run of rsync."
+	rsync -rnvc --delete -e "ssh -i $HOME/.ssh/id_rsa" ~/Sites/Kevin\ Yap/output/ keviny_kevinyap@ssh.phx.nearlyfreespeech.net:/home/public/
 
-	echo -n "Confirm upload via rsync (y/n): "
+	printf "\e[0;36mConfirm upload via rsync (y/n): \e[0m"
 	read confirm
 
 	if [ $confirm == 'y' ]; then
-		echo "rsync confirmed."
+		rsync -rc --delete -e "ssh -i $HOME/.ssh/id_rsa" ~/Sites/Kevin\ Yap/output/ keviny_kevinyap@ssh.phx.nearlyfreespeech.net:/home/public/
+		printf "\e[0;32mOutput directory updated using rsync.\e[0m\n"e
+		exit
 	else
-		echo "rsync terminated."
+		printf "\e[0;31mrsync terminated.\e[0m\n"
 		exit
 	fi
-
-	rsync -r --delete -e "ssh -i $HOME/.ssh/id_rsa" ~/Sites/Kevin\ Yap/output/ keviny_kevinyap@ssh.phx.nearlyfreespeech.net:/home/public/
-	echo "Output directory updated using rsync."
 fi
 
 # Note to self: update ~/.bash_profile file with 'alias generate="bash ~/Sites/Kevin\ Yap/generate.sh"'
