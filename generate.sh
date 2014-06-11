237b61362a2e9bb2d5b60d85a87b670c89ab978a
Kevin Yap <me@kevinyap.ca>
2014-06-10 22:04:03 -0700

    Cleaned up Bash scripts

--- generate.sh
+++ generate.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 upload() {
-	bash deploy.sh
+	bash "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/deploy.sh
 }
 
 develop() {
 	# Determine local IP http://stackoverflow.com/a/13322549/239076
 	localIP=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
+	rootPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 	developPath=${rootPath}/develop
 
 	trap killgroup SIGINT
@@ -15,20 +16,16 @@ develop() {
 		kill 0
 	}
 
+	cd $rootPath
 	(pelican -rs pelicanconf.py) &
-	(sleep 2; cd $developPath; printf "Serving HTTP at \e[1;37m${localIP}:8000\e[0m.\n";
+	(sleep 1.5; cd $developPath; printf "Serving HTTP at \e[1;37m${localIP}:8000\e[0m.\n";
 		python -m SimpleHTTPServer 8000 1>/dev/null) &
 	wait
 }
 
-rootPath="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
-cd $rootPath
-
 case $1 in
-	"-d"|"-p")
-		develop ;;
-	"-u")
-		upload ;;
+	-d|-p) develop ;;
+	-u) upload ;;
 	*)
 		read -n1 -p "Development or upload site? [D/U] " input
 		printf "\n"
