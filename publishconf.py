845603e81aeced7485557b485630a128d3c6a9f1
Kevin Yap <me@kevinyap.ca>
2014-06-14 16:38:23 -0700

    Minor theme file modifications

--- publishconf.py
+++ publishconf.py
@@ -11,6 +11,7 @@ from pelicanconf import *
 SITEURL = 'http://kevinyap.ca'
 RELATIVE_URLS = False
 OUTPUT_PATH = 'output/'
+
 DISQUS_SITENAME = 'kevinyap'
 GOOGLE_ANALYTICS = 'UA-41937659-1'
 DOMAIN = 'kevinyap.ca'
