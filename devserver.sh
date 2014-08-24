2f91910068ead33be1e8e661f94603e9244fd2e9
Kevin Yap <me@kevinyap.ca>
2014-08-24 12:21:37 -0700

    Modified devserver.sh logic

--- devserver.sh
+++ devserver.sh
@@ -1,20 +1,15 @@
-#!/bin/bash
-
-trap killgroup SIGINT
-killgroup() {
-	cd $rootPath && rm -rf develop && echo "Deleted develop directory successfully."
-	kill 0
-}
-
+#!/usr/bin/env bash
 rootPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 developPath=${rootPath}/develop
-
-cd $rootPath
+port=8000
 
 # Local IP function: http://stackoverflow.com/a/13322549/239076
 localIP=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
+serveMsg="Serving HTTP at \e[1;37m${localIP}:${port}\e[0m."
+
+trap "cd $rootPath && rm -r develop && kill 0" SIGINT
+cd $rootPath && pelican -s pelicanconf.py > /dev/null # seed directory with site content
 
 (pelican -rs pelicanconf.py) &
-(sleep 2; cd $developPath; printf "Serving HTTP at \e[1;37m${localIP}:8000\e[0m.\n";
-	python -m SimpleHTTPServer 8000 1>/dev/null) &
+(cd $developPath; echo -e $serveMsg; python -m SimpleHTTPServer $port 1> /dev/null) &
 wait
